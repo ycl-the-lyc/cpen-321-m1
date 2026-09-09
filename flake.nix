f461c995cceeab0b5a8b075ee6ce94ad29a3facb
ycl-the-lyc <steve.lyc.2980@gmail.com>
2026-09-09 13:56:29 -0700

    env setup

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "CPEN 321 Milestone 1";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    fl = flake-utils.lib;
+  in
+    fl.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # TS
+          nodejs
+          typescript
+          typescript-language-server
+          # Kotlin
+          openjdk17
+          kotlin
+          gradle
+          kotlin-language-server
+          # Andriod
+          android-studio-tools
+          # Docker
+          docker
+        ];
+      };
+    });
+}
